ca2050dade8938d537c97c85eb17d59b87a610c0
SooriyaaK <s.karunaharan@student.vu.nl>
2026-04-14 17:29:25 +0200

    alll updated files

--- Comparison.py
+++ Comparison.py
@@ -1,6 +1,8 @@
 # Comparison
 import pandas as pd
 
+# Using quantile - Random Forest 50 vs previous approvh 87%
+# Now we dont have NAN values for XGBoost
 rf_results = pd.read_csv("DataMining/csv_files/rf_results.csv")
 xgb_results = pd.read_csv("DataMining/csv_files/xgb_results.csv")
 
