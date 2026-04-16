eb3d7394f84e982968867a45fe5770e3e17057ef
Gileesa McCormack <gileesa@icloud.com>
2026-04-16 12:38:53 +0200

    Cleaned up code

--- data_cleaning.py
+++ data_cleaning.py
@@ -18,7 +18,7 @@ TRIMMING:bool = False
 # when changing this, don't forget to 
 # change the data cleaning steps at the end of 
 # this document as well !!
-RELEVANT_FEATURES = ['mood', 'screen', 'circumplex.valence', 'circumplex.arousal', 'activity', 'appCat.social', 'appCat.game', 'appCat.entertainment']
+RELEVANT_FEATURES = ['mood', 'screen', 'circumplex.valence', 'circumplex.arousal', 'activity', 'appCat.social', 'appCat.entertainment']
 
 
 
@@ -236,103 +236,6 @@ def apply_KNN_imputation(
     result = pd.concat(cleaned_dfs, ignore_index=True)
     return result
 
-    
-
-
-def clean_time_series(
-    df,
-    varname='mood',
-    max_gap=3, #max gap of days
-    trimming=True,
-    plotting: bool=False
-):
-    '''
-    Function that can impute missing date values for 'varname'. 
-    In addition, this function cuts away parts on the head and tail of the timeseries
-    that have a gap of >3 days with the 'main body' of the timeseries.
-    '''
-
-    df = df[df['variable'] == varname].copy()
-    df['date_day'] = df['date'].dt.floor('D')
-    cleaned_dfs = []
-    for uid, user_df in df.groupby('id'):
-
-        #compute daily average and std
-        user_daily = (
-            user_df
-            .groupby('date_day')['value']
-            .agg(value='mean', value_std='std')
-            .sort_index()
-        )
-        if user_daily.empty:
-            # skip rest of loop
-            continue 
-        elif plotting:
-            # plot histogram of values to see range
-            plot_histogram(user_daily, varname, uid)
-
-        # find range of dates
-        full_range = pd.date_range(
-            user_daily.index.min(),
-            user_daily.index.max(),
-            freq='D'
-        )
-        user_full = user_daily.reindex(full_range)
-
-        # trim head and tail of data if there is a large gap
-        is_valid = ~user_full['value'].isna()
-        valid_idx = np.where(is_valid)[0] #find dates with data
-
-        if len(valid_idx) == 0:
-            continue
-
-        # find distance (data-wise) between existing data points
-        distance_between_data = np.diff(valid_idx) 
-        split_points = np.where(distance_between_data > max_gap)[0]
-
-        # split data in continuous chunks
-        # split at positions in split_points
-        segments = []
-        start = 0
-        for sp in split_points:
-            segments.append(valid_idx[start:sp+1])
-            start = sp + 1
-        segments.append(valid_idx[start:])
-
-        # trim isolated head/tail segments, keep everything in between 
-        # only remove the first segment if it is separated from the rest by a large gap 
-        # only remove the last segment if it is separated from the rest by a large gap
-        if trimming:
-            # trim head/tail
-            largest_segment = max(segments, key=len)
-
-            start_idx = largest_segment[0]
-            end_idx = largest_segment[-1]
-
-            user_trimmed = user_full.iloc[start_idx:end_idx+1]
-        else:
-            # no trimming
-            user_trimmed = user_full.copy()
-
-        # impute missing values (mean only; std stays NaN for imputed days)
-        user_imputed = user_trimmed.copy()
-        user_imputed['value'] = user_trimmed['value'].interpolate(method='time') # linear interpol with actual time spacing
-
-        # print imputations for debug
-        imputed_mask = user_trimmed['value'].isna() & user_imputed['value'].notna()
-        for date, val in user_imputed.loc[imputed_mask, 'value'].items():
-            print(f"[IMPUTED] ID={uid} | date={date.date()} | value={val:.3f}")
-
-        # create dfs
-        clean_df = pd.DataFrame({
-            'id': uid,
-            'date': user_imputed.index,
-            'value': user_imputed['value'].values,
-            'value_std': user_imputed['value_std'].values  # NaN for imputed days
-        })
-        cleaned_dfs.append(clean_df)
-    result = pd.concat(cleaned_dfs, ignore_index=True)
-    return result
 
 
 def plot_cleaned_per_id(df_clean, original_df, feature: str = 'mood', save_path=None):
@@ -403,85 +306,8 @@ def plot_cleaned_per_id(df_clean, original_df, feature: str = 'mood', save_path=
 
 
 
-
-def create_n_size_window(
-    dfs,                
-    feature_names=['screen', 'mood'],   
-    window_size=5,
-    target_feature='mood',
-    save_path='csv_files/window_dataset.csv'
-):
-    """
-    Generalized sliding window dataset for multiple features.
-
-    params:
-    - dfs: list of cleaned dataframes
-    - feature_names: list of feature names (same length as dfs)
-    - target_feature: which feature to predict (default = mood)
-    """
-
-    merged_df = None
-
-    for df, feature in zip(dfs, feature_names):
-        # rename columns (temporarily)
-        temp = df[['id', 'date', 'value']].rename(columns={'value': feature})
-
-        if merged_df is None:
-            # if nothing to merge into, become temp
-            merged_df = temp
-        else:
-            # merge dataframes
-            merged_df = pd.merge(
-                merged_df,
-                temp,
-                on=['id', 'date'],
-                how='inner' # keep only dates where all features exist
-            )
-
-    merged_df = merged_df.sort_values(['id', 'date'])
-
-    all_rows = []
-
-    # create windows 
-    for uid, user_df in merged_df.groupby('id'):
-        user_df = user_df.sort_values('date').reset_index(drop=True)
-
-        if len(user_df) < window_size + 1:
-            continue
-
-        for i in range(window_size, len(user_df)):
-            row = {}
-
-            # id + date
-            row['id'] = uid
-            row['date'] = user_df.loc[i, 'date']
-
-            # loop over all features
-            for feature in feature_names:
-                for j in range(window_size):
-                    # create window values
-                    row[f'{feature}_t-{window_size-j}'] = user_df.loc[
-                        i - window_size + j, feature 
-                    ]
-
-            # target
-            row['target'] = user_df.loc[i, target_feature]
-
-            all_rows.append(row)
-
-    dataset = pd.DataFrame(all_rows)
-
-    # save to csv
-    dataset.to_csv(save_path, index=False)
-    print(f"\nSaved dataset to: {save_path}")
-    print(f"Shape: {dataset.shape}")
-
-    return dataset
-
-
-
 def create_one_size_window(
-    dfs,
+    df,
     feature_names=['screen', 'mood'],
     window_size=5,
     target_feature='mood',
@@ -490,9 +316,13 @@ def create_one_size_window(
 
     merged_df = None
 
-    for df, feature in zip(dfs, feature_names):
+    for feature in feature_names:
+        # debug
+        ids_with_feature = df[df['variable'] == feature]['id'].unique()
+        print(f"{feature}: {len(ids_with_feature)}/27 users")
+
         # rename columns (temporarily)
-        temp = df[['id', 'date', 'value']].rename(columns={'value': feature})
+        temp = df[df['variable'] == feature][['id', 'date', 'value']].rename(columns={'value': feature})
 
         if merged_df is None:
             # if nothing to merge into, become temp
@@ -544,6 +374,8 @@ def create_one_size_window(
 
     return dataset
 
+
+### ==== MAIN CLEANING PART ========
 df = pd.read_csv('dataset_mood_smartphone.csv')
 
 #  make datetime
@@ -554,8 +386,6 @@ cleaned_df = missing_imputation(df, 'circumplex.valence', linear_interpol)
 cleaned_df = missing_imputation(cleaned_df, 'circumplex.arousal', linear_interpol)
 # still one value missing in circumplex.valence; try backward interpol
 cleaned_df = missing_imputation(cleaned_df, 'circumplex.valence', backward_interpol)
-print('\n\n====== IMPUTED MISSING ROW VALUES HEAD =======\n', cleaned_df.head(20))
-print(cleaned_df.head(20))
 print_general_info(cleaned_df)
 
 #==== remove extreme values, replace with neighbour average (linear interpol) ======
@@ -565,38 +395,20 @@ cleaned_df = remove_extremes(cleaned_df, varname='appCat.game', max_value=1000)
 cleaned_df = remove_extremes(cleaned_df, varname='appCat.entertaiment', max_value=1000)
 
 #====== add missing dates =========
-# + cut off parts of timeseries with large gaps
-clean_dates_mood = clean_time_series(cleaned_df)
-clean_dates_screentime = clean_time_series(cleaned_df, varname = 'screen')
-clean_dates_valence = clean_time_series(cleaned_df, varname = 'circumplex.valence')
-clean_dates_arousal = clean_time_series(cleaned_df, varname = 'circumplex.arousal')
-clean_dates_activity = clean_time_series(cleaned_df, varname = 'activity')
-clean_dates_social = clean_time_series(cleaned_df, varname = 'appCat.social')
-clean_dates_game = clean_time_series(cleaned_df, varname = 'appCat.game')
-clean_dates_entertainment = clean_time_series(cleaned_df, varname = 'appCat.entertainment')
-
 print('DOING KNN imputation')
 KNN_impute = apply_KNN_imputation(cleaned_df, 3, RELEVANT_FEATURES)
-
-print('\n\n====== IMPUTED DATES HEAD =======\n', clean_dates_mood.head(20))
 plot_cleaned_per_id(KNN_impute, cleaned_df, save_path='Figures/KNN/dates_mood.png')
 plot_cleaned_per_id(KNN_impute, cleaned_df, feature='screen', save_path='Figures/KNN/dates_screen.png')
 plot_cleaned_per_id(KNN_impute, cleaned_df, feature='circumplex.valence', save_path='Figures/KNN/dates_valence.png')
 plot_cleaned_per_id(KNN_impute, cleaned_df, feature='circumplex.arousal', save_path='Figures/KNN/dates_arousal.png')
 plot_cleaned_per_id(KNN_impute, cleaned_df, feature='activity', save_path='Figures/KNN/dates_activity.png')
 
-
-df_windows = create_n_size_window(
-    [clean_dates_mood, clean_dates_screentime, clean_dates_valence, clean_dates_arousal, clean_dates_activity, clean_dates_social, clean_dates_game, clean_dates_entertainment],
-    feature_names=['mood', 'screen', 'circumplex.valence', 'circumplex.arousal', 'activity', 'appCat.social', 'appCat.game', 'appCat.entertainment'],
+# Create window, averaging n days
+df_windows = create_one_size_window(
+    KNN_impute,
+    feature_names=RELEVANT_FEATURES,
     window_size=5,
-    save_path='csv_files/mood_window_dataset2.csv'
+    save_path='csv_files/KNN/KKN_one_size_mood_window_dataset.csv'
 )
-print(df_windows.head(20))
 
-df_windows2 = create_one_size_window(
-    [clean_dates_mood, clean_dates_screentime, clean_dates_valence, clean_dates_arousal, clean_dates_activity, clean_dates_social, clean_dates_game, clean_dates_entertainment],
-    feature_names=['mood', 'screen', 'circumplex.valence', 'circumplex.arousal', 'activity', 'appCat.social', 'appCat.game', 'appCat.entertainment'],
-    window_size=5,
-    save_path='csv_files/one_size_mood_window_dataset.csv'
-)
+print('WINDOW number of ids: ', len(df_windows['id'].unique()))
