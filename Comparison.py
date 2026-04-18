736911b3a6e8000c371df42f3f745fc24a5af201
SooriyaaK <s.karunaharan@student.vu.nl>
2026-04-18 23:56:18 +0200

    added test version reduced features to 20

--- Comparison.py
+++ Comparison.py
@@ -1,8 +1,8 @@
 # Comparison
 import pandas as pd
 
-rf_results = pd.read_csv("DataMining/csv_files/RandomForest/rf_results.csv")
-xgb_results = pd.read_csv("DataMining/csv_files/XGBOOST/xgb_results.csv")
+rf_results = pd.read_csv("DataMining/csv_files/Classification/RandomForest/rf_results.csv")
+xgb_results = pd.read_csv("DataMining/csv_files/Classification/XGBOOST/xgb_results.csv")
 
 summary = pd.concat([rf_results, xgb_results], ignore_index=True)
 print(summary.to_string(index=False))
